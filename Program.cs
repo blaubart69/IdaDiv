1c67c916153164260037a616a6e9915a3be71779
Bernhard Spindler <bernhard.spindler@unicredit.at>
2023-04-25 11:01:55 +0200

    before dividend iterator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 using System.Text;
 using System;
 using System.Linq;
@@ -17,40 +17,48 @@ class Division
         }
         else
         {
-            Divide(dividend, Convert.ToInt32(divisor), out string result, out List<string> temps);
-            Console.WriteLine($"{dividend} : {divisor} = {result}"); 
-            foreach( string temp in temps )
-            {
-                Console.WriteLine(  temp);
-            }
+            Divide(dividend, Convert.ToInt32(divisor), out string result, out StringBuilder temps);
+            Console.WriteLine("{0} : {1} = {2}\n{3}",
+                dividend
+                ,divisor
+                ,result
+                ,temps); 
+            
         }
 
         return rc;
     }
-    static void Divide(string dividend, int divisor, out string result, out List<string> tempResults)
+    static void Divide(string dividend, int divisor, out string result, out StringBuilder tempResults)
     {
+        //var dividendIter = dividend.AsSpan().GetEnumerator();
         //
         // finden wieviel Stellen gebraucht werden bis der Divisor reinpasst
         //
-        int stelle = DigitCount(divisor);
-        string blanks;
-        if ( Convert.ToInt32(dividend.Substring(0,stelle)) < divisor )
+        int lenDivisor = DigitCount(divisor);
+        int padCount = lenDivisor;
+
+        int idxDividend = lenDivisor - 1;
+        int spaces;
+        //
+        // sind die ersten Ziffern (lenDivisor) kleiner als der Divisor selbst?
+        //
+        if ( Convert.ToInt32(dividend.Substring(0,lenDivisor)) < divisor )
         {
-            stelle += 1;
-            blanks = " ";
+            // dann nehmen wir noch eine Stelle vom Dividend dazu
+            //  für die erste Division
+            idxDividend += 1;
+            spaces = 1;
         }
         else
         {
-            blanks = "";
+            spaces = 0;
         }
 
-        string strRest = dividend.Substring(0,stelle);
+        string strRest = dividend.Substring(0,idxDividend+1);
         
         result = String.Empty;
-        tempResults = new List<string>();
-        stelle -= 1;
+        tempResults = new StringBuilder();
 
-        int padCount = DigitCount(divisor);
         do
         {
             //
@@ -61,22 +69,21 @@ class Division
 
             strRest = rest.ToString();
             strRest = strRest.PadLeft(padCount,'0');
-            ++stelle;
+            ++idxDividend;
 
-            if ( stelle < dividend.Length )
+            if ( idxDividend < dividend.Length )
             {
-                strRest += dividend[stelle];
+                strRest += dividend[idxDividend];
             }
             else
             {
                 strRest += "R";
             }
-
-            
-            tempResults.Add(blanks + strRest);
-            blanks += " ";
+            tempResults.Append    (new string(' ', spaces));
+            tempResults.AppendLine(strRest);
+            ++spaces;
         }
-        while (stelle < dividend.Length);
+        while (idxDividend < dividend.Length);
     }
 
     static int DigitCount(long val)
